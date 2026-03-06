cee20892052b85edbc340e633fa5b3617f1fa31f
Sang <sangtruong2804.work@gmail.com>
2026-03-06 12:11:56 +0700

    first commit

---
+++ src/test/java/com/sangtq28/ojt_be/OjtBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangtq28.ojt_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjtBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
